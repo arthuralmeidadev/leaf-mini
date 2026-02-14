db527ba32e87550d1b5ff48f415ba14ba88d3c88
arthuralmeidadev <arthuralmeida.office.dev@gmail.com>
2026-02-14 14:50:49 -0300

    ref/simplify processor loader

--- src/pipeline/loader.rs
+++ src/pipeline/loader.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{io::Error, path::PathBuf, sync::Arc};
 
 use crate::pipeline::{
     config::PipelineConfig,
@@ -8,18 +8,11 @@ use crate::pipeline::{
 pub fn load_file_processors(
     input_dir: &PathBuf,
     config: Arc<PipelineConfig>,
-) -> Result<Vec<Box<dyn ProcessData + Send + Sync>>, std::io::Error> {
+) -> Result<Vec<Box<dyn ProcessData + Send + Sync>>, Error> {
     let entries = input_dir.read_dir()?;
 
-    Ok(entries.fold(Vec::new(), |mut acc, entry| {
-        let Ok(entry) = entry else {
-            return acc;
-        };
-
-        if let Some(file_entry) = FileProcessor::new(entry.path(), Arc::clone(&config)) {
-            acc.push(file_entry);
-        };
-
-        acc
-    }))
+    Ok(entries
+        .flatten()
+        .filter_map(|entry| FileProcessor::new(entry.path(), Arc::clone(&config)))
+        .collect())
 }
